0ce1b2cb997a8aba248d866c784f784749a310cf
UrsineRaven <UrsineRaven@gmail.com>
2019-06-28 13:02:26 -0500

    fixing script to support files in folders

--- .local/bin/pull_dotfiles.sh
+++ .local/bin/pull_dotfiles.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # This script pulls UrsineRaven's dotfiles, and checks them out to the 
 # appropriate places. It will backup any pre-existing dotfiles to 
 # $HOME/.dotfiles-backup
 #
 # NOTE: This script assumes that you have git installed.
+# 
+# Sources: https://www.atlassian.com/git/tutorials/dotfiles
 
 git clone --bare https://github.com/UrsineRaven/dotfiles.git $HOME/.dotfiles
 function dotfiles {
@@ -15,7 +17,7 @@ if [ $? = 0 ]; then
 else
 	echo "Backing up pre-existing dot files."
 	mkdir $HOME/.dotfiles-backup
-	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p "$HOME/.dotfiles-backup/$(dirname {}); mv $HOME/{} $HOME/.dotfiles-backup/{} ;'
 	dotfiles checkout
 fi
 dotfiles config --local status.showUntrackedFiles no
